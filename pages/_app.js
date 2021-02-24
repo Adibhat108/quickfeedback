bcd76c1a104dc1d5b8d087497b1e3f1e7d00b8f2
AdiBhat108 <jadithyabhat@gmail.com>
2021-02-24 21:22:10 +0530

    add firebase auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+// import App from 'next/app'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { AuthProvider } from "../lib/auth";
+
+const App = ({ Component, pageProps }) => {
+  return <AuthProvider>
+    <Component {...pageProps} />
+  </AuthProvider>;
 }
 
-export default MyApp
+export default App;
