4c65368e56f1b9c0e583ad74f74d3153bd3fe735
AdiBhat108 <jadithyabhat@gmail.com>
2021-02-25 23:23:24 +0530

    Firestore, Chakra UI, Absolute Imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,43 @@
 // import App from 'next/app'
 
-import { AuthProvider } from "../lib/auth";
+import { CSSReset, ThemeProvider } from '@chakra-ui/core';
+import { css, Global } from '@emotion/core';
+
+import { AuthProvider } from '@/lib/auth';
+import customTheme from '@/styles/theme';
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            min-width: 360px;
+            scroll-behavior: smooth;
+          }
+
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 const App = ({ Component, pageProps }) => {
-  return <AuthProvider>
-    <Component {...pageProps} />
-  </AuthProvider>;
-}
+  return (
+  <ThemeProvider theme={customTheme}>
+    <AuthProvider>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </AuthProvider>
+  </ThemeProvider>
+  );
+};
 
 export default App;
--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,30 @@
 import Head from 'next/head'
-import { useAuth } from '../lib/auth'
-import styles from '../styles/Home.module.css'
+import { Button, Code, Heading, Text } from '@chakra-ui/core';
+
+import { useAuth } from '@/lib/auth'
 
 export default function Home() {
   const auth = useAuth();
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Fast Feedback</title>
+        <title>Quick Feedback</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main>
+        <Heading>
           Quick Feedback
-        </h1>
+        </Heading>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-        
+        <Text> current user: <Code>{auth?.user?.email}</Code></Text>
         {auth.user ? (
-          <button onClick={(e) => auth.signinWithGithub()}>Sign In</button>
-          ): (
-          <button onClick={(e) => auth.signout()}>Sign Out</button>
+          <Button onClick={(e) => auth.signout()}>Sign Out</Button>
+        ): (
+          <Button onClick={(e) => auth.signinWithGithub()}>Sign In</Button>
         )}
-        <div>{auth?.user?.email}</div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
